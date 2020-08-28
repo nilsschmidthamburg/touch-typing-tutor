124a3a3ccbaad329b5872808aec495a393282cef
Nils Schmidt <nils.schmidt@mailbox.org>
2020-08-28 13:36:07 +0200

    Updates

---
+++ precache-manifest.75b034fa176f6935a3ea65cb462ae880.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7370ee0779c9659df9c8d8cc6ec372e6",
+    "url": "/touch-typing-tutor/index.html"
+  },
+  {
+    "revision": "c1d5d2ba905ea4ac56a7",
+    "url": "/touch-typing-tutor/static/css/main.8d51c12f.chunk.css"
+  },
+  {
+    "revision": "b06fe507c9713ed0f427",
+    "url": "/touch-typing-tutor/static/js/2.ed6762fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/touch-typing-tutor/static/js/2.ed6762fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1d5d2ba905ea4ac56a7",
+    "url": "/touch-typing-tutor/static/js/main.6dc84544.chunk.js"
+  },
+  {
+    "revision": "4746238011ff30fc7f5a",
+    "url": "/touch-typing-tutor/static/js/runtime-main.efb12113.js"
+  }
+]);
